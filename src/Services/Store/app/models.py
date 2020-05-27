8a6d7eb04e11ab291ec77db705da0cda907d74b3
fychin <fyang96c@gmail.com>
2020-05-27 21:32:16 +0800

    Add store model and migration files

---
+++ src/Services/Store/app/models.py
@@ -0,0 +1,27 @@
+from app import db
+
+class Store(db.Model):
+    __tablename__ = 'store'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    address = db.Column(db.String())
+    country = db.Column(db.String())
+
+    def __init__(self, name, address, country):
+        self.name = name
+        self.address = address
+        self.country = country
+
+    def __repr(self):
+        return '<Store_id: {}, country: {}'.format(self.id, self.country)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'address': self.address,
+            'country': self.country
+        }
+
+ 
